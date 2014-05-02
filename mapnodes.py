cec777821d4f28b3fa9f2920d13fffcbe4ae54c0
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2014-05-02 14:06:32 +0200

    tweet the result!

--- mapnodes.py
+++ mapnodes.py
@@ -1,10 +1,13 @@
 
 from sys import argv
+from datetime import datetime
 from requests import get as rget
 from changeffapi import Loader
 
 NODESJSON = 'http://map.freifunk-mainz.de/nodes.json'
 FFAPIJSON = 'ffapi_file.json'
+
+TWEETRESULT = True
 
 def scrape(url):
     '''returns remote json'''
@@ -19,7 +22,6 @@ if __name__ == '__main__':
     if nodes:
         online = 0
         nonclient = 0
-        loader = Loader(FFAPIJSON)
 
         for node in nodes['nodes']:
             if node['flags']['online']:
@@ -27,9 +29,16 @@ if __name__ == '__main__':
                 if not node['flags']['client']:
                     nonclient += 1
 
-            if len(argv) > 1:
-                print('Status: online: %d -> [nonclient: %d, client: %d]' %(online, nonclient, online-nonclient))
-            else:
-                if nonclient != int(loader.find(['state', 'nodes'])):
-                    loader.set(['state', 'nodes'], nonclient)
-                    loader.dump(overwrite=True)
+        now = datetime.now().strftime('%H:%M %d.%m.%Y')
+        resultmsg = 'Status: online: %d (%d Router, %d Teilnehmer) %s' %(online, nonclient, online-nonclient, now)
+        if len(argv) > 1:
+            print(resultmsg)
+        else:
+            loader = Loader(FFAPIJSON)
+            if nonclient != int(loader.find(['state', 'nodes'])):
+                loader.set(['state', 'nodes'], nonclient)
+                loader.dump(overwrite=True)
+
+            if TWEETRESULT:
+                from notify.twitter import send_tweet
+                send_tweet(resultmsg)

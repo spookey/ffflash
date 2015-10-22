4f74925308735c5bee291d6cad1380c1dfc1168c
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-10-22 20:40:57 +0200

    Rewrite Storage, use furhther Element to track changes

--- ffflash/store.py
+++ ffflash/store.py
@@ -1,23 +1,23 @@
 from ffflash import log, now, timeout
-from ffflash.lib.container import Container
 from ffflash.lib.clock import epoch_repr
+from ffflash.lib.container import Container
+from ffflash.lib.data import Element
 
 
 class Storage(Container):
     def __init__(self, storage):
-        super(Storage, self).__init__('storage', storage)
-        self.data['nodes'] = self.data.get('nodes', {})
-        self.changes = {}
+        super().__init__('nodes', storage)
+        self.changes = Element()
 
-    def _prepare_all(self):
-        clean = {}
+    def _info(self):
+        super()._info()
 
-        for node_id, data in self.data['nodes'].items():
-            hostname = data.get('hostname')
-            last_seen = data.get('last_seen')
-            if last_seen and last_seen > timeout:
-                clean[node_id] = {}
-                for name, val in data.items():
+    def __prepare_all(self):
+        clean = Element()
+        for node_id, data in self.data.items():
+            node = Element(data)
+            if node.last_seen and node.last_seen > timeout:
+                for name, val in node.items():
                     if name.startswith('_'):
                         continue
                     elif name in ['online']:
@@ -26,94 +26,76 @@ class Storage(Container):
                         val = None
                     clean[node_id][name] = val
             else:
-                self.changes['lost'] = self.changes.get('lost', {})
-                self.changes['lost'][node_id] = {
-                    'hostname': hostname, 'new': None, 'old': last_seen
+                self.changes.lost[node_id] = {
+                    'hostname': node.hostname,
+                    'new': None, 'old': node.last_seen
                 }
-                log.info('node change lost {}'.format(hostname))
+                log.info('lost node {}'.format(node.hostname))
 
-        self.data['nodes'] = clean
+        self.data = clean
 
-    def _new(self, node_id, hostname):
-        if node_id not in self.data['nodes'].keys():
-            log.info('node change new {}'.format(hostname))
-            self.data['nodes'][node_id] = {
+    def __node_new(self, node_id, node):
+        if node_id not in self.data.keys():
+            self.data[node_id] = Element({
                 'first_seen': now,
                 'reboots': 0,
                 'uptime': 0
+            })
+            self.changes.new[node_id] = {
+                'hostname': node.hostname,
+                'new': now, 'old': None
             }
-            self.changes['new'] = self.changes.get('new', {})
-            self.changes['new'][node_id] = {
-                'hostname': hostname, 'new': now, 'old': None
+            log.info('new node {}'.format(node.hostname))
+
+    def __node_current(self, node_id, node):
+        old_uptime = self.data[node_id].get('uptime', 0)
+        if node.uptime and old_uptime > node.uptime:
+            self.data[node_id].reboots += 1
+            self.changes.reboots[node_id] = {
+                'hostname': node.hostname,
+                'new': node.uptime, 'old': old_uptime
             }
+            log.info('reboot node {}'.format(node.hostname))
+        self.data[node_id].uptime = node.uptime
+        self.data[node_id].last_seen = now
+        self.data[node_id].online = True
 
-    def _current(self, node_id, hostname, uptime):
-        self.data['nodes'][node_id]['last_seen'] = now
-        self.data['nodes'][node_id]['online'] = True
-
-        old_uptime = self.data['nodes'][node_id].get('uptime', 0)
-        if uptime and old_uptime > uptime:
-            self.data['nodes'][node_id]['reboots'] += 1
-            self.changes['reboots'] = self.changes.get('reboots', {})
-            self.changes['reboots'][node_id] = {
-                'hostname': hostname, 'new': uptime, 'old': old_uptime
-            }
-
-    def _generic(self, node_id, hostname, name, val):
-        old = self.data['nodes'][node_id].get(name)
-        self.data['nodes'][node_id][name] = val
+    def __node_generic(self, node_id, node, key, val):
+        old = self.data[node_id].get(key)
+        self.data[node_id][key] = val
         if old and old != val:
-            log.info('node change {} {}: {} {}'.format(
-                name, hostname, old, val
-            ))
-            self.changes[name] = self.changes.get(name, {})
-            self.changes[name][node_id] = {
-                'hostname': hostname, 'new': val, 'old': old
+            self.changes[key][node_id] = {
+                'hostname': node.hostname,
+                'new': val, 'old': old
             }
+            log.info('{} node change {}'.format(key, node.hostname))
 
-    def _extend(self, node_id, hostname):
-        node = self.data['nodes'][node_id]
-        first_seen = node.get('first_seen')
-        last_seen = node.get('last_seen')
-        uptime = node.get('uptime')
-        gateway = node.get('gateway')
-
-        if uptime:
-            self.data['nodes'][node_id]['_uptime'] = epoch_repr(
-                uptime, ms=True
+    def __node_extend(self, node_id, node):
+        node = self.data[node_id]
+        if node.uptime:
+            self.data[node_id]._uptime = epoch_repr(node.uptime, ms=True)
+        if node.last_seen and node.first_seen:
+            self.data[node_id]._lifespan = epoch_repr(
+                abs(node.last_seen - node.first_seen), ms=True
             )
-        if first_seen and last_seen:
-            self.data['nodes'][node_id]['_lifespan'] = epoch_repr(
-                abs(last_seen - first_seen), ms=True
-            )
-        if gateway:
-            gw = self.data['nodes'].get(gateway.replace(':', ''))
-            if gw:
-                self.data['nodes'][node_id]['_gateway'] = gw.get(
-                    'hostname'
-                )
-
-    def _gen_info(self):
-        info = {}
-        return info
+        if node.gateway:
+            gateway = self.data.get(node.gateway.replace(':', ''))
+            if gateway:
+                self.data[node_id]._gateway = gateway.get('hostname')
 
     def update(self, fresh):
-        self.changes = {}
+        self.changes = Element()
 
-        self._prepare_all()
+        self.__prepare_all()
 
-        for node_id, data in fresh:
-            hostname = data.get('hostname')
-            uptime = data.get('uptime')
-            self._new(node_id, hostname)
-            self._current(node_id, hostname, uptime)
-            for name, val in data.items():
-                self._generic(node_id, hostname, name, val)
-            self._extend(node_id, hostname)
+        for node_id, node in fresh:
+            self.__node_new(node_id, node)
+            self.__node_current(node_id, node)
+            for key, val in node.items():
+                self.__node_generic(node_id, node, key, val)
+            self.__node_extend(node_id, node)
 
-        log.info('update done')
-        if self.data['nodes']:
-            info = self._gen_info()
-            self.save(info=info)
+        log.info('update finished')
+        self.save()
 
         return self.changes

6be2b914bed1b76295801a5bc8e8319d94ef8229
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-10-22 20:37:31 +0200

    Add Element data type, wrapping dict for easy access

--- ffflash/lib/data.py
+++ ffflash/lib/data.py
@@ -35,3 +35,25 @@ def merge_dicts(left, right):
                 deepcopy(right[key])
             )
     return res
+
+
+class Element(dict):
+    def __getattr__(self, key):
+        return self.__getitem__(key)
+
+    def __getitem__(self, key):
+        value = super().__getitem__(key)
+        if isinstance(value, dict):
+            value = Element(value)
+            self.__setitem__(key, value)
+        return value
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __delattr__(self, key):
+        self.__delitem__(key)
+
+    def __missing__(self, key):
+        self.__setitem__(key, Element())
+        return self.__getitem__(key)

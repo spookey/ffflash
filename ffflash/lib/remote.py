8e8e79d9e2cf4c29572d9c784fc9e2da52cb7457
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-11-24 23:04:35 +0100

    Modify www_fetch to enable mokeypatched testing simulation

--- ffflash/lib/remote.py
+++ ffflash/lib/remote.py
@@ -1,14 +1,12 @@
 from contextlib import contextmanager
 from socket import gaierror
-from urllib.error import HTTPError, URLError
-from urllib.request import urlopen
+from urllib import error, request
 
 
 @contextmanager
 def www_fetch(url, fallback=None):
     try:
-        resp = urlopen(url)
+        resp = request.urlopen(url)
         yield resp.read().decode('utf-8')
-    except (HTTPError, URLError, gaierror):
+    except (error.HTTPError, error.URLError, gaierror, ValueError):
         yield fallback
-    return
---
+++ tests/lib/remote/test_www_fetch.py
@@ -0,0 +1,31 @@
+from urllib import request
+
+from ffflash.lib.remote import www_fetch
+
+
+def test_www_fetch_wrong_urls():
+    for wu in ['', '2342', '/']:
+        with www_fetch(wu) as t:
+            assert t is None
+
+
+def test_www_fetch_fallback():
+    for wu in ['', '2342', '/']:
+        for fb in [None, True, False, 'wrong', {}]:
+            with www_fetch(wu, fallback=fb) as t:
+                assert t == fb
+
+
+def test_www_decode_patched_results(monkeypatch):
+    def r(u):
+        class R:
+            def read(self):
+                return bytes(u, 'utf-8')
+
+        return R()
+
+    monkeypatch.setattr(request, 'urlopen', r)
+
+    for td in ['test', '🐸', '\n\t', ' ']:
+        with www_fetch(td) as t:
+            assert t == td

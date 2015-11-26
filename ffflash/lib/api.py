6f6d1a7146020121a98ba6bb6cfa718686fc2a8d
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2015-11-26 20:19:19 +0100

    Fix double indentation error in FFApi class

--- ffflash/lib/api.py
+++ ffflash/lib/api.py
@@ -5,31 +5,31 @@ from re import sub as re_sub
 
 
 class FFApi:
-        def __init__(self, content):
-            self.c = content
+    def __init__(self, content):
+        self.c = content
 
-        def pull(self, *fields):
-            c = self.c
-            for f in fields:
-                if isinstance(c, dict) and f in c.keys():
-                    if f == fields[-1]:
-                        return c[f]
-                    c = c[f]
+    def pull(self, *fields):
+        c = self.c
+        for f in fields:
+            if isinstance(c, dict) and f in c.keys():
+                if f == fields[-1]:
+                    return c[f]
+                c = c[f]
 
-        def push(self, value, *fields):
-            c = self.c
-            for f in fields:
-                if isinstance(c, dict) and f in c.keys():
-                    if f == fields[-1]:
-                        c[f] = value
-                    c = c[f]
+    def push(self, value, *fields):
+        c = self.c
+        for f in fields:
+            if isinstance(c, dict) and f in c.keys():
+                if f == fields[-1]:
+                    c[f] = value
+                c = c[f]
 
-        def timestamp(self):
-            if self.pull('state', 'lastchange') is not None:
-                self.push(api_timestamp(), 'state', 'lastchange')
+    def timestamp(self):
+        if self.pull('state', 'lastchange') is not None:
+            self.push(api_timestamp(), 'state', 'lastchange')
 
-        def show(self):
-            return pformat(self.c)
+    def show(self):
+        return pformat(self.c)
 
 
 def api_timestamp(dt=None):
